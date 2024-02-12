5ddd055e083aeffb6eb41fbf59b11f78376abaca
Nicholas Kow <nicholas.kow08@gmail.com>
2024-02-12 20:40:20 +0800

    Added Responsiveness to website

--- Template.js
+++ Template.js
@@ -1,5 +1,13 @@
-const logobutton = document.getElementById("Logobutton");
-logobutton.addEventListener('onclick', function () {
-  logobutton.textContent = "Amongus";
-  
-});
+
+function showdiv(){
+  var div = document.getElementById("menu-toggle-page");
+  div.style.transition = "all 1s ease";
+  div.style.visibility = "visible";
+  div.style.transform = "translateY(0px)";
+}
+function hidediv(){
+  var div = document.getElementById("menu-toggle-page");
+  div.style.transition = "all 1s ease";
+  div.style.visibility = "hidden";
+  div.style.transform = "translateY(calc(5*10vh*-1))";
+}
